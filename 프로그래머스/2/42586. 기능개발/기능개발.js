4a76c75d270532ea7b17936c2849f34e05692f18
SuMin <ksm9805@naver.com>
2025-02-20 17:01:51 +0900

    [level 2] Title: 기능개발, Time: 3.82 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,21 @@
+function solution(progresses, speeds) {
+    let day = progresses.map((i,idx) => Math.ceil((100-i)/speeds[idx]))
+    let standard = day[0]
+    let result=[]
+    let cnt = 1
+    for(let i = 1; i<day.length; i++){
+        if( day[i]<= standard ){
+            cnt++
+        } else {
+            result.push(cnt)
+            standard=day[i]
+            cnt = 1
+        }
+        if(i === day.length-1){
+            result.push(cnt)
+        }
+        
+    }
+    console.log(day)
+    return result;
+}
