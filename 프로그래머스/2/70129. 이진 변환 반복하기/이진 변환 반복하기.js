39c015852a4f095765af451dec9770ea535c6332
SuMin <ksm9805@naver.com>
2025-02-20 14:09:58 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.36 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,10 +1,12 @@
 function solution(s) {
     let zeroCnt = 0
     let totalCnt = 1
+    
     function fn(s){
         let newArr = s.split('')
         let filtered = newArr.filter((i) => Number(i) === 1)
-        zeroCnt += newArr.length-filtered.length
+        
+        zeroCnt += newArr.length - filtered.length
         
         if(filtered.length !== 1 && filtered[0] !== 1){
             totalCnt++
