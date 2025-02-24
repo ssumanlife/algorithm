8b643b0706069684619f9f912e94c35c5f010441
SuMin <ksm9805@naver.com>
2025-02-24 11:43:10 +0900

    [level 1] Title: 과일 장수, Time: 290.03 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,12 @@
+function solution(k, m, score) {
+    let arr = []
+    score.sort((a,b) => b-a)
+    const newScore = score.slice(0,m*Math.floor(score.length/m))
+
+    for(let i = 0; i < newScore.length; i+=m){
+        let box = newScore.slice(i,i+m).sort((a,b) => a-b)
+        arr.push(box[0]*m)
+    }
+
+    return arr.length !== 0 ? arr.reduce((a,b) => a+b) : 0
+}
