cd5513f9a63c6f7acebcbc72653217343cb0fd7b
SuMin <ksm9805@naver.com>
2025-02-24 12:08:50 +0900

    [level 1] Title: 소수 찾기, Time: 52.84 ms, Memory: 45 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let arr = Array(n+1).fill(true)
+    arr[0] = arr[1] = false
+    
+    for(let i = 2; i*i<=n; i++){
+        if(arr[i]){
+            for(let j = i*i; j<=n; j+=i){
+                arr[j] = false
+            }
+        }
+    }
+    return arr.filter(Boolean).length
+}
