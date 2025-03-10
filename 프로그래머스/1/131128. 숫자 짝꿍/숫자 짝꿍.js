40f7140855aedbc1587f4169cc002791dcf2733a
SuMin <ksm9805@naver.com>
2025-03-10 13:14:27 +0900

    [level 1] Title: 숫자 짝꿍, Time: 217.07 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,18 @@
+function solution(X, Y) {
+    let cntX = {}, cntY={}
+    
+    for(let num of X) cntX[num] = (cntX[num] || 0) + 1
+    for(let num of Y) cntY[num] = (cntY[num] || 0) + 1
+    
+    let result = []
+    
+    for(let i = 9; i>=0; i--){
+        if(cntX[i] && cntY[i]){
+            result.push(String(i).repeat(Math.min(cntX[i], cntY[i])))
+        }
+    }
+
+    if(result.length ===0) return "-1"
+    if(result.join('') === "0".repeat(result.join('').length)) return "0"
+    return result.join('')
+}
