f1fcce61e6ddad17e85b676a346d445814ca0358
SuMin <ksm9805@naver.com>
2025-03-19 12:24:47 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 2.42 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,29 @@
+function solution(board, moves) {
+    let basket = []
+    let arr = board.map(b => [...b])
+    
+    for(let i =0; i<moves.length; i++){
+        let doll = arr.map((d) => d[moves[i] - 1])
+        let idx = doll.findIndex((a) => a !== 0)
+        if(idx !== -1){
+            basket.push(doll[idx])
+            arr[idx][moves[i]-1] = 0
+        }
+    }
+    
+    let cnt = 0
+    let i = 0
+    
+    
+    while(i< basket.length - 1){
+        if(basket[i] === basket[i + 1]){
+            cnt +=1
+            basket.splice(i,2)
+            i = Math.max(0, i-1)
+        }else {
+            i++
+        }
+    }
+    
+    return cnt*2;
+}
